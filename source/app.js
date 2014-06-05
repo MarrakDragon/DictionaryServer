85c115b63b384d4003824af3b6a119dfe13edf1d
Jeff Brown <jeff@dragonkeep.org>
2014-06-05 15:15:17 -0700

    Updated to load a dictionary

--- source/app.js
+++ source/app.js
@@ -42,7 +42,7 @@ app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 // Dictionary Load for DAG
-//dictionary.loadDictionary('public\\dictionaries\\enable1.txt');
+dictionary.loadDictionary('public\\dictionaries\\enable1.txt');
 
 // development only
 if ('development' === app.get('env')) {
